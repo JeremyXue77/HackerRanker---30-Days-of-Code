f6c73d4be2005845c90a8d1285d833d98c1d1842
Jeremy Xue <gg831006@gmail.com>
2018-10-02 22:42:16 +0800

    Update Day 1- Data Type Answer

---
+++ Day 1- Data Types/Day 1- Data Types.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var secondInt: Int
+var secondDouble: Double
+var secondString: String
+// Read and save an integer, double, and String to your variables.
+secondInt = Int(readLine()!)!
+secondDouble = Double(readLine()!)!
+secondString = readLine()!
+// Print the sum of both integer variables on a new line.
+print(i + secondInt)
+// Print the sum of the double variables on a new line.
+print(d + secondDouble)
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s + secondString)
